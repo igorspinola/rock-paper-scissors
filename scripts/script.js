eb926be4a8f030406dd6f67a7d67075f5b4f56a6
Igor <igorspinoladev@gmail.com>
2023-07-08 00:17:26 -0300

    Simple UI added

--- scripts/script.js
+++ scripts/script.js
@@ -1,74 +1,128 @@
 const options = ['ROCK', 'PAPER', 'SCISSORS']
+
 function getComputerChoice() {
-    const index = Math.floor(3 * (Math.random()))
-    return options[index]
+  const index = Math.floor(3 * (Math.random()))
+  return options[index]
 }
 
 function round(playerSelection, computerSelection) {
   const playerOption = playerSelection.toUpperCase()
 
-if (playerOption === computerSelection) {
-  return 'It is a draw. Both are the same.'
-} 
-else if (playerOption === 'ROCK' && computerSelection === 'PAPER') {
-  return 'You LOST! Paper beats Rock!'
-} 
-else if (playerOption === 'ROCK' && computerSelection === 'SCISSORS') {
-  return 'You WON! Rock beats Scissors!'
+  if (playerOption === computerSelection) {
+    return 'It is a draw. Both are the same.'
+  } 
+  else if (playerOption === 'ROCK' && computerSelection === 'PAPER') {
+    return 'You LOST! Paper beats Rock!'
+  } 
+  else if (playerOption === 'ROCK' && computerSelection === 'SCISSORS') {
+    return 'You WON! Rock beats Scissors!'
+  }
+  else if (playerOption === 'PAPER' && computerSelection === 'ROCK') {
+    return 'You WON! Paper beats Rock!'
+  } 
+  else if (playerOption === 'PAPER' && computerSelection === 'SCISSORS') {
+    return 'You LOST! Scissors beats Paper'
+  }
+  else if (playerOption === 'SCISSORS' && computerSelection === 'ROCK') {
+    return 'You LOST! Rock beats Scissors!'
+  } 
+  else if (playerOption === 'SCISSORS' && computerSelection === 'PAPER') {
+    return 'You WON! Scissors beats Paper!'
+  }
+  else {
+    return 'Invalid Option!'
+  }
 }
-else if (playerOption === 'PAPER' && computerSelection === 'ROCK') {
-  return 'You WON! Paper beats Rock!'
-} 
-else if (playerOption === 'PAPER' && computerSelection === 'SCISSORS') {
-  return 'You LOST! Scissors beats Paper'
-}
-else if (playerOption === 'SCISSORS' && computerSelection === 'ROCK') {
-  return 'You LOST! Rock beats Scissors!'
-} 
-else if (playerOption === 'SCISSORS' && computerSelection === 'PAPER') {
-  return 'You WON! Scissors beats Paper!'
-}
-else {
-  return 'Invalid Option!'
-}
-}
+const display = document.createElement('div')
+const container= document.querySelector('.displayResults')
 
-function game(playerOptions) {
+document.querySelector('.button1').addEventListener('click', () => {
+  const result = round('ROCK' ,getComputerChoice())
+  console.log(result)
+  if (playerScore === 0 && computerScore === 0 ) {
+    playerSc.textContent = `Player Score: ${playerScore} `
+    computerSc.textContent = `Computer Score: ${computerScore} `
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+  }
+  const show = document.createElement('p')
+  show.textContent = result
+  display.appendChild(show)
+  countScore(result)
+})
+
+document.querySelector('.button2').addEventListener('click', () => {
+  const result = round('PAPER' ,getComputerChoice())
+  console.log(result)
+  if (playerScore === 0 && computerScore === 0 ) {
+    playerSc.textContent = `Player Score: ${playerScore} `
+    computerSc.textContent = `Computer Score: ${computerScore} `
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+  }
+  const show = document.createElement('p')
+  show.textContent = result
+  display.appendChild(show)
+  countScore(result)
+})
+
+document.querySelector('.button3').addEventListener('click', () => {
+  const result = round('SCISSORS' ,getComputerChoice())
+  console.log(result)
+  if (playerScore === 0 && computerScore === 0 ) {
+    playerSc.textContent = `Player Score: ${playerScore} `
+    computerSc.textContent = `Computer Score: ${computerScore} `
+    while (display.firstChild) {
+      display.removeChild(display.firstChild);
+    }
+  }
+  const show = document.createElement('p')
+  show.textContent = result
+  display.appendChild(show)
+  countScore(result)
+})
+
 let playerScore = 0
 let computerScore = 0
-  for (let i = 0; i < 5; i++) {
-  const result = round(playerOptions[i], getComputerChoice())
-  console.log(result)
+const playerSc = document.querySelector('.playerScore')
+const computerSc = document.querySelector('.computerScore')
+
+function countScore (result) {
+  container.append(display)
   if (result === 'You WON! Rock beats Scissors!' || result === 'You WON! Paper beats Rock!' ||result === 'You WON! Scissors beats Paper!') {
-  playerScore++
-  console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
-}
+    playerScore++
+    playerSc.textContent = `Player Score: ${playerScore} `
+    console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
+  }
   else if (result === 'You LOST! Paper beats Rock!' || result === 'You LOST! Scissors beats Paper' || result === 'You LOST! Rock beats Scissors!') {
-  computerScore++
-  console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
-} 
+    computerScore++
+    computerSc.textContent = `Computer Score: ${computerScore} `
+    console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
+  } 
   else if (result === 'Invalid Option!') {
-  alert("Invalid Game, play again!")
-} 
-else if (result === 'It is a draw. Both are the same.') {
-  console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
-}
-
-}
-  if (playerScore === computerScore) {
-    return 'It is a draw!'
-}
-  else if (playerScore > computerScore) {
+    alert("Invalid Game, play again!")
+  } 
+  else if (result === 'It is a draw. Both are the same.') {
+    console.log(`Player score is ${playerScore}, computer Score is ${computerScore}`)
+  }
+  
+  if (playerScore === 5) {
+    const show = document.createElement('p')
+    show.textContent = 'You won the game!'
+    display.appendChild(show)
+    playerScore = 0
+    computerScore = 0
     return 'You won the game!'
-}
-  else {
+  }
+  else if (computerScore === 5) {
+    const show = document.createElement('p')
+    show.textContent = 'You lost the game!'
+    display.appendChild(show)
+    playerScore = 0
+    computerScore = 0
     return 'You lost the game!'
+  }
+  
 }
-}
-
-const arr = []
-for (let i = 1; i <= 5; i++) {
-  const choice = prompt(`What is your choice for round ${i}? Choose beetween Rock, Paper or Scissors`)
-  arr.push(choice)
-}
-console.log(game(arr))
